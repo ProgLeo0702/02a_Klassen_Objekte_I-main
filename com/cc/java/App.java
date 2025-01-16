a604617ff86b898a13b1d73fcc40efaec511fee3
ProgLeo0702 <leon-ahner@gmx.de>
2025-01-16 11:15:54 +0100

    Iteration Instanziierung

---
+++ com/cc/java/App.java
@@ -0,0 +1,29 @@
+package com.cc.java;
+
+public class App {
+    
+    public static void main(String[] args) {
+
+        /*
+        *   Instanziierung --> Erzeugen eines Objektes durch Aufruf eines Konstruktors
+        *   Objekt <-----> Instanz
+        *   cat --> Referenzvariable
+        *   Cat --> Referenztyp (Klasse)
+        */
+
+
+
+
+        Cat cat = new Cat();
+        System.out.println(cat);
+
+        System.out.println("---------------------");
+
+        Cat cat2 = new Cat();
+        System.out.println(cat2);
+    }
+
+   
+
+}
+
